538f7e1b1535aa6620fa99564f73e8806e3ddc2c
POOJAELANGO03 <amsapooja@gmail.com>
2025-06-29 18:55:05 +0530

    NEW ONE

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -141,7 +141,7 @@ const Projects: React.FC = () => {
                 <p className="project-description">{project.description}</p>
                 <div className="project-tech">
                   {project.technologies.map(tech => (
-                    <span key={tech} className="tech-tag">{tech}</span>
+                    <span key={tech} className="tech-tag">{tech}</span>//ONU PURILA
                   ))}
                 </div>
               </div>
